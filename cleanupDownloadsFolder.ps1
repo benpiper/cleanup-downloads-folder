f9fc05e1aa3430b06ab3a0f499264b746b5b4626
Ben PipeR <ben@benpiper.com>
2022-01-30 15:17:05 -0500

    updates

--- cleanupDownloadsFolder.ps1
+++ cleanupDownloadsFolder.ps1
@@ -1,10 +1,10 @@
 $mindays = 60
 $minsize = "0KB"
-$downloadspath = "~/downloads"  # Will recurse into subdirectories
+$downloadspath = @("~/downloads", "~/desktop")  # Will recurse into subdirectories
 $extensions = @("*.exe", "*.msi", "*.zip", "*.iso", "*.7z", "*.gz", "*.tmp", "*.qcow2", "*.mp3", "*.pem")
 #$extensions = @("*")  # For all files
 $paranoid = $false  # Wipe free space after deleting files
-$WhatIfPreference = $false  # Safety switch to prevent accidentally deleting files. Set to $false to have the script delete files for real.
+$WhatIfPreference = $true  # Safety switch to prevent accidentally deleting files. Set to $false to have the script delete files for real.
 
 $files = Get-ChildItem -Include $extensions -Path $downloadspath -Recurse
 $filesToDelete = $files | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-$mindays))} | Where-Object {$_.Length -gt $minsize} 
@@ -13,5 +13,5 @@ Write-Host $filesToDelete.Count files will be deleted.
 $filesToDelete | Remove-Item
 
 if ($paranoid) {
-    cipher /w:$downloadspath &
+    cipher.exe /w:$downloadspath &
 }
---
+++ readme.md
@@ -0,0 +1,31 @@
+# cleanup-downloads-folder
+
+PowerShell script to clean up your messy Windows folders.
+
+## Usage
+
+Edit the configuration parameters in <cleanupDownloadsFolder.ps1>.
+
+`$mindays = 60`
+
+The minimum age of files to delete.
+
+`$minsize = "0KB"`
+
+The minimum size of files to delete.
+
+`$downloadspath = @("~/downloads", "~/desktop")`
+
+The path(s) to delete files from. The script will recurse into subdirectories.
+
+`$extensions = @("*.exe", "*.msi", "*.zip", "*.iso", "*.7z", "*.gz", "*.tmp", "*.qcow2", "*.mp3", "*.pem")`
+
+File extensions to include. To include all files, set `#$extensions = @("*")`
+
+`$paranoid = $false`
+
+Uses `cipher.exe` to wipe free space after deleting files.
+
+`$WhatIfPreference = $true`
+
+Safety switch to prevent accidentally deleting files. Set to `$false` to have the script delete files for real.
