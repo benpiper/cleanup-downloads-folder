73c19cb5641747ce5aa7c157de5c9c768550aa2c
Ben PipeR <ben@benpiper.com>
2022-01-30 00:18:24 -0500

    updates

--- cleanupDownloadsFolder.ps1
+++ cleanupDownloadsFolder.ps1
@@ -1,8 +1,17 @@
-$maxdays = 730
-$maxsize = "100KB"
-$downloadspath = "~/downloads"
+$mindays = 60
+$minsize = "0KB"
+$downloadspath = "~/downloads"  # Will recurse into subdirectories
+$extensions = @("*.exe", "*.msi", "*.zip", "*.iso", "*.7z", "*.gz", "*.tmp", "*.qcow2", "*.mp3", "*.pem")
+#$extensions = @("*")  # For all files
+$paranoid = $false  # Wipe free space after deleting files
+$WhatIfPreference = $false  # Safety switch to prevent accidentally deleting files. Set to $false to have the script delete files for real.
 
-$files = Get-ChildItem -Path $downloadspath -Recurse
-$filesToDelete = $files | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-$maxdays))} | Where-Object {$_.Length -gt $maxsize} 
-Write-Host The following files will be deleted $filesToDelete
-$filesToDelete | Remove-Item -WhatIf
+$files = Get-ChildItem -Include $extensions -Path $downloadspath -Recurse
+$filesToDelete = $files | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-$mindays))} | Where-Object {$_.Length -gt $minsize} 
+Write-Host Matching files: $filesToDelete.Name | Sort-Object
+Write-Host $filesToDelete.Count files will be deleted.
+$filesToDelete | Remove-Item
+
+if ($paranoid) {
+    cipher /w:$downloadspath &
+}
